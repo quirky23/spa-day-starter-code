f42e7f04a6144a4b2cb4e33abe1b7e18ff8e835e
Courtney Phillips <quirkyfiddie@yahoo.com>
2021-03-04 20:54:37 -0600

    -m "finished Studio"

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,32 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Cannot be blank!")
+    @Size(min=5, max=15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message= "Password is required.")
+    @Size(min=6, message = "Must be at least 6 characters.")
     private String password;
 
-    public User() {
+    private String passwordVerify;
 
-    }
+    public User() { }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String passwordVerify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.passwordVerify = passwordVerify;
     }
 
     public String getUsername() {
@@ -38,5 +51,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getPasswordVerify() {
+        return passwordVerify;
+    }
+
+    public void setPasswordVerify(String passwordVerify) {
+        this.passwordVerify = passwordVerify;
     }
 }
